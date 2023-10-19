ae342991007c80b2c6457e167fe1ea5d68899719
Stacy Rouse Estrada Malpartida <srestradam01@educastillalamancha.es>
2023-10-19 10:18:47 +0200

    Prueba de unión de ramas

--- GestionarFicheros/src/main/java/com/mycompany/gestionarficheros/LecturaSecuencial.java
+++ GestionarFicheros/src/main/java/com/mycompany/gestionarficheros/LecturaSecuencial.java
@@ -5,7 +5,9 @@
 package com.mycompany.gestionarficheros;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -40,6 +42,57 @@ public class LecturaSecuencial {
             System.out.println("NO se encontró el fichero");
         } catch (IOException iex) {
             System.out.println("ERROR E/S");
+        }
+    }
+    
+    /**
+     * Método para leer de manera secuencial un archivo (binario)
+     * 
+     * @param ruta lugar donde se encuentra el archivo a leer
+     * @param nombre nombre del archivo
+     */
+    public static void leerSecuencialBin(String ruta, String nombre){
+        File fichero=new File(ruta+"//"+nombre);
+        try (FileInputStream fis = new FileInputStream(fichero);
+                DataInputStream dis = new DataInputStream(fis)) {
+            
+            
+            int indice;
+            int puntuacion;
+            String lugar;
+            double precio;
+            String nombrePlato;
+            
+                while (fis.available() > 0) {
+                    indice = dis.readInt();
+                    puntuacion = dis.readInt();
+                    lugar = "";
+                    for (int i=0; i<10; i++) {
+                        lugar += dis.readChar();
+                    }
+                    precio = dis.readDouble();
+                    nombrePlato = "";
+                    for (int i=0; i<20; i++) {
+                        nombrePlato += dis.readChar();
+                    }
+
+                    System.out.print(indice);
+                    System.out.print(" ");
+                    System.out.print(puntuacion);
+                    System.out.print(" ");
+                    System.out.print(lugar);
+                    System.out.print(" ");
+                    System.out.print(precio);
+                    System.out.print(" ");
+                    System.out.print(nombrePlato);
+                    System.out.println();
+                }
+            fis.close();
+            dis.close();
+        }catch(FileNotFoundException fnfe){
+            System.out.println("NO se encontró el fichero");
+        } catch (IOException iex) {
+            System.out.println("Lectura terminada...");
         }
     }
 }
